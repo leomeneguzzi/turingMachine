fc452ba9f6574b091bf31caf5728fb720b15bb0b
Leonardo Meneguzzi <meneguzzileo@gmail.com>
2017-08-27 19:50:43 -0300

    temporario

--- src/Index.ts
+++ src/Index.ts
@@ -1,5 +1,16 @@
+import {Transitions} from './Transitions';
+import { Transition } from './Transition';
+
 $('.form').submit(function (event){
     event.preventDefault();
     let _transitions = $('#transitions');
-    console.log(_transitions.val());
+    //console.log([].concat((_transitions.val() as string).split(/\r?\n/)));
+    let transitionsInput = [].concat((_transitions.val() as string).split(/\r?\n/));
+
+    let transitions = new Transitions();
+    transitionsInput.forEach(transition => {
+        //(...transitions.split(','))
+        transitions.push(new Transition(...transition.split(',')));
+    });
+    console.log(transitions.toArray());
 });
