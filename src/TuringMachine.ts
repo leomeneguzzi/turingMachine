a9cf158020a6abce59bb361be1dac9c26daa78f6
Leonardo Meneguzzi <meneguzzileo@gmail.com>
2017-08-28 15:27:56 -0300

    Ajuste de um if.

--- src/TuringMachine.ts
+++ src/TuringMachine.ts
@@ -7,18 +7,15 @@ export class TuringMachine{
         if(transition != undefined) {
             inputTape[positionTape] = transition.write;
             positionTape += transition.directionToInt();
-            if(transition.targetState != 'H'){
-                state = transition.targetState;
-                return this.testTape(inputTape, transitions, positionTape, state);
-            }else{
-                return true;
-            }
+            state = transition.targetState;
+            return transition.targetState != 'H' ? 
+            this.testTape(inputTape, transitions, positionTape, state) : true;
         }else{
             return false;
         }
     }
 
-    static testTransitions(transitions : Array < Transition >, tapeValue : string) : Transition{
+    private static testTransitions(transitions : Array < Transition >, tapeValue : string) : Transition{
         return transitions.find((transition : Transition) => transition.read == tapeValue);
     }
 }
